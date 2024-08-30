0be64b3f9cdd589946fdadad04e31e9c9b012205
Vedprakash Sharma <sharmavedprakash9128@gmail.com>
2024-08-30 18:14:51 +0530

    GET nids and emp_name using Service

--- custom/employee/src/Controller/NodeApiController.php
+++ custom/employee/src/Controller/NodeApiController.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
- *
+ * Controller for handling API requests related to nodes.
  */
 class NodeApiController extends ControllerBase {
 
@@ -19,7 +19,13 @@ class NodeApiController extends ControllerBase {
   }
 
   /**
+   * Creates an instance of this controller.
    *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container.
+   *
+   * @return static
+   *   The controller instance.
    */
   public static function create(ContainerInterface $container) {
     return new static(
@@ -28,10 +34,10 @@ class NodeApiController extends ControllerBase {
   }
 
   /**
-   * Returns details of all nodes created through a custom form.
+   * Returns details of nodes including nid and emp_name.
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
-   *   A JSON response containing the node details.
+   *   A JSON response containing node IDs and employee names.
    */
   public function getNodes() {
     $nodes_data = [];
@@ -40,27 +46,14 @@ class NodeApiController extends ControllerBase {
     $nodes = $this->employeeService->getNodesByType('employee_form');
 
     foreach ($nodes as $node) {
-      $node_data = [
-        'id' => $node->id(),
-        'title' => $node->getTitle(),
-        'type' => $node->getType(),
-        'created' => date('Y-m-d H:i:s', $node->getCreatedTime()),
+      $nid = $node->id(); // Node ID (nid)
+      $emp_name = $node->hasField('field_emp_name') ? $node->get('field_emp_name')->value : NULL;
+
+      $nodes_data[] = [
+        'nid' => $nid,       // Node ID
+            // Alias for Node ID (optional, same as 'nid')
+        'emp_name' => $emp_name, // Employee name
       ];
-
-      // Extract additional fields.
-      if ($node->hasField('field_emp_name') && !$node->get('field_emp_name')->isEmpty()) {
-        $node_data['emp_name'] = $node->get('field_emp_name')->value;
-      }
-
-      if ($node->hasField('field_emp_age') && !$node->get('field_emp_age')->isEmpty()) {
-        $node_data['emp_age'] = $node->get('field_emp_age')->value;
-      }
-
-      if ($node->hasField('field_emp_email') && !$node->get('field_emp_email')->isEmpty()) {
-        $node_data['emp_email'] = $node->get('field_emp_email')->value;
-      }
-
-      $nodes_data[] = $node_data;
     }
 
     return new JsonResponse($nodes_data);
